b12b7cb6246d2112fa4126834493ebeac3de0b10
Priyank Gupta <priyaaank@gmail.com>
2011-01-20 19:58:58 +0530

    Added bpositive webservice project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BpositiveWs::Application.config.secret_token = 'b502c6a59955db90c65ff44949727b79741b7824a6a43351e58cb60404e2dbc6533d30b37a60a9ddd3508a0aead17fafca4054891951c75873ff15914a88148c'
