dbde50cb626b99f7ab56c5a75f96306bc3192739
Priyank <priyaaank@gmail.com>
2011-01-21 16:14:01 +0530

    Added more specs for the domain objects

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper'
 
 describe Address do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "attributes" do
+    it "should respond to address line 1" do
+      Address.new.should respond_to(:address_line1)
+    end
+
+    it "should respond to address line 2" do
+      Address.new.should respond_to(:address_line2)
+    end
+
+    it "should respond to state" do
+      Address.new.should respond_to(:state)
+    end
+
+    it "should respond to city" do
+      Address.new.should respond_to(:city)
+    end
+
+    it "should respond to country" do
+      Address.new.should respond_to(:country)
+    end
+
+    it "should respond to zip_code" do
+      Address.new.should respond_to(:zip_code)
+    end
+
+    it "should respond to latitude" do
+      Address.new.should respond_to(:latitude)
+    end
+
+    it "should respond to longitude" do
+      Address.new.should respond_to(:longitude)
+    end
+  end
+
 end
+
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Donation do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "attributes" do
+    it "should respond to date of donation" do
+      Donation.new.should respond_to(:date_of_donation)
+    end
+  end
+
 end
